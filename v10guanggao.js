fcc7c19709f127e71bc14b7c5d33157ccbd8b446
admincj <77110599+admincaiji@users.noreply.github.com>
2021-09-18 02:01:12 +0800

    Create v10guanggao.js

---
+++ v10guanggao.js
@@ -0,0 +1,22 @@
+
+var v10guanggao = {
+    'advs': {
+             'head': '广告位',
+             'tips': '<font color="red">此区为广告位投放广告请联系飞机：viphsdy</font>',
+             'rows': [
+             {
+                     'url1': 'https://www.siwazyw.tv',
+                     'tip1': '丝袜资源网丝袜资源网',
+                     'url2': 'https://www.siwazyw.tv',
+                     'tip2': '丝袜资源网丝袜资源网',
+                     'url3': 'https://www.siwazyw.tv',
+                     'tip3': '丝袜资源网丝袜资源网',
+                     'url4': 'https://www.siwazyw.tv',
+                     'tip4': '丝袜资源网丝袜资源网',
+                     'url5': 'https://www.siwazyw.tv',
+                     'tip5': '丝袜资源网丝袜资源网',
+                 },
+               
+                 ]
+                 },	
+}; 
